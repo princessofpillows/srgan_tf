6570b39f15670b25c76a405145f150e7edd23a52
Jordan <jpatts@uvic.ca>
2019-04-16 14:11:11 -0700

    add image logger

--- srgan.py
+++ srgan.py
@@ -88,7 +88,12 @@ class SRGAN(object):
 
             # Log a generated image
             tf.contrib.summary.image('SRGAN/generated', images)
-            
+    
+    def log_img(self, img, name):
+        if self.global_step.numpy() % (cfg.log_freq * 5) == 0:
+            with tf.contrib.summary.always_record_summaries():
+                img = tf.cast(img, tf.float32)
+                tf.contrib.summary.image(name, img, max_images=3)
 
     def update(self, hr_crop, hr_ds):
         # Construct graph
